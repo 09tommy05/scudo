a224b93aa8e015ad588d859bfa107f3ad0af6eaa
09tommy05 <tommaso.malinverno@studenti.unitn.it>
2026-01-16 16:24:07 +0100

    feat: 404 and 500 error handlers

--- backend/app/app.js
+++ backend/app/app.js
@@ -60,4 +60,14 @@ app.use('/api/v1/report-answers/', tokenChecker, rbac("reporter", "user"), repor
 
 app.use("/api/v1/operators/", tokenChecker, rbac("admin"), operators);
 
+//404 and 500 error handlers
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'La rotta richiesta non esiste' });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Errore interno del server' });
+});
+
 export default app;

9ee6e8afed2aea8fd58f47ae212d243bc5494eaf
09tommy05 <tommaso.malinverno@studenti.unitn.it>
2026-02-18 16:50:26 +0100

    added PWA support

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 const app = createApp(App)
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,30 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    VitePWA({
+          registerType: 'autoUpdate',
+          manifest: {
+            name: 'SCUDO',
+            short_name: 'SCUDO',
+            description: 'Servizio Comunale Unico per la Difesa Online',
+            theme_color: '#1D58BF',
+            background_color: '#ffffff',
+            display: 'standalone',
+            start_url: '/',
+            icons: [
+              {
+                src: '/icons/pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+              },
+              {
+                src: '/icons/pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png'
+              }
+            ]
+          }
+        })
   ],
   resolve: {
     alias: {

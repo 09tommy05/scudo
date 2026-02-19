159cca00ee5dcd89c57b7232e1dd4d9713bc348e
09tommy05 <tommaso.malinverno@studenti.unitn.it>
2026-02-19 18:07:12 +0100

    fix-frontend: pwa, scudo.pdf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,29 +13,32 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
     VitePWA({
-          registerType: 'autoUpdate',
-          manifest: {
-            name: 'SCUDO',
-            short_name: 'SCUDO',
-            description: 'Servizio Comunale Unico per la Difesa Online',
-            theme_color: '#1D58BF',
-            background_color: '#ffffff',
-            display: 'standalone',
-            start_url: '/',
-            icons: [
-              {
-                src: '/icons/pwa-192x192.png',
-                sizes: '192x192',
-                type: 'image/png'
-              },
-              {
-                src: '/icons/pwa-512x512.png',
-                sizes: '512x512',
-                type: 'image/png'
-              }
-            ]
+      registerType: 'autoUpdate',
+      workbox: {
+        navigateFallbackDenylist: [/^\/.*\.pdf$/]
+      },
+      manifest: {
+        name: 'SCUDO',
+        short_name: 'SCUDO',
+        description: 'Servizio Comunale Unico per la Difesa Online',
+        theme_color: '#1D58BF',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           }
-        })
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
